2ce9e1e75d85495bae73722af1281e3e4b848a16
Magiskandev <hello@mail.com>
2020-12-13 23:50:58 -0500

    fcc project.

---
+++ src/pages/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Main.css';
+import App from '../components/App';
+
+function Main() {
+  return (
+    <div className='drum-pad'>
+      <App/>
+    </div>
+  );
+}
+
+export default Main;
